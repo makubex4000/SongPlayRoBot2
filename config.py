bb0a79785c2785af8aebfc65d6d7c42a66f2c080
KevinChen <67411136+makubex2010@users.noreply.github.com>
2023-08-12 21:35:45 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ import re
 from yt_dlp import YoutubeDL
 
 class Config:
-    API_ID = "2710398"
-    API_HASH = "e64c45a8f94ae642c090404e5f81f196"
-    BOT_TOKEN = "1800777638:AAGMKMECKMCVolKCFkC5T-3SkudIjsFtYTE"
-    START_MSG = "<b>嗨! {}, 我是 🎸歌曲播放機器人，</b>發送你想要的歌名或網址... 😍🥰🤗例如:/s 南拳媽媽-下雨天"
-    START_IMG = "https://telegra.ph/file/d0cdeca30d2082b437645.jpg"
-    OWNER = "Kevin_RX"
-    DOWNLOAD_LOCATION = "DOWNLOAD_LOCATION", "./DOWNLOADS/"
+    API_ID = int(os.environ.get("API_ID"))
+    API_HASH = os.environ.get("API_HASH")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    START_MSG = os.environ.get("START_MSG")
+    START_IMG = os.environ.get("START_IMG")
+    OWNER = os.environ.get("OWNER") 
+    DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/")
     msg = {}
