61d5b194cb5c547d58040968ed62103f7ddc3d74
makubex4000 <98877740+makubex4000@users.noreply.github.com>
2024-03-03 16:55:40 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from yt_dlp import YoutubeDL
 class Config:
     API_ID = "2710398"
     API_HASH = "e64c45a8f94ae642c090404e5f81f196"
-    BOT_TOKEN = "1800777638:AAF_U0_UtG8YTCSen5JmFBBLvZHLCXq9Vv4"
+    BOT_TOKEN = "1800777638:AAFoY5LOTs8f7ulIRYgBltAl3oJswHBzWyQ"
     START_MSG = "<b>嗨! {}, 我是 🎸歌曲播放機器人，</b>發送你想要的歌名或網址... 😍🥰🤗例如:/s 南拳媽媽-下雨天"
     START_IMG = "https://telegra.ph/file/d0cdeca30d2082b437645.jpg"
     OWNER = "Kevin_RX"
